fb19937ca0e6f1b94c216c8e882b7937dbeb46e8
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-04-25 23:03:33 -0700

    Fixed tweet_consumer.py

--- tweet_consumer.py
+++ tweet_consumer.py
@@ -4,16 +4,15 @@ from kafka import KafkaConsumer
 if __name__ == "__main__":
     topic = input("Enter topic: ")
     try:
-        consumer = KafkaConsumer( topic,
-                         group_id='consumer-group',
-                         bootstrap_servers=['localhost:9092'],
-                         auto_offset_reset='earliest',
-                         enable_auto_commit=False,
-                         value_deserializer=lambda m: json.loads(m.decode('asciii')),
-                         consumer_timeout_ms=1000)
+        consumer = KafkaConsumer(topic,
+                                 group_id='consumer-group',
+                                 bootstrap_servers=['localhost:9092'],
+                                 auto_offset_reset='earliest',
+                                 enable_auto_commit=False,
+                                 value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                                 consumer_timeout_ms=1000)
         for tweet in consumer:
-            print (tweet.topic, "value:", tweet.value)
+            print(tweet.topic, "value:", tweet.value)
 
     except Exception as ex:
         print("Error")
-
