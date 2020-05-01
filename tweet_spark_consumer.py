9036170810cef1730b3175665cbc3832c69ba8b0
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-05-01 16:45:03 -0700

    Updated tweet_spark_consumer.py

--- tweet_spark_consumer.py
+++ tweet_spark_consumer.py
@@ -4,20 +4,28 @@ from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+from kafka import KafkaConsumer
+
 import tweet_listener
 
 
 def sentiment_analysis():
     pass
 
+consumer = KafkaConsumer(tweet_listener.brand,
+                                 group_id='consumer-group',
+                                 bootstrap_servers=['localhost:9092'],
+                                 auto_offset_reset='earliest',
+                                 enable_auto_commit=False,
+                                 value_deserializer=lambda m: json.loads(m.decode('ascii')),
+                                 consumer_timeout_ms=1000)
 
 conf = SparkConf().setMaster("local[*]").setAppName("TwitterStream")
-sc = SparkContext()
+sc = SparkContext(conf=conf)
 ssc = StreamingContext(sc, 5)
 topic = tweet_listener.brand
 kafka_stream = KafkaUtils.createStream(ssc, "localhost:2181", "consumer-group", {topic: 1})
 lines = kafka_stream.map(lambda x: json.loads(x[1]))
-#lines.foreachRDD(sentiment_analysis)
 lines.pprint()
 ssc.start()
 ssc.awaitTermination()
