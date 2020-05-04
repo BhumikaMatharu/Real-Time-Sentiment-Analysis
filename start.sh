cbf3c1d66b74409b0fc70c8568f64561042d8fd9
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-05-03 22:08:12 -0700

    Updates start.sh to detach the screens

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu/apache/kafka_2.12-2.5.0/bin/
-screen -S zookeeper -m sudo ./zookeeper-server-start.sh /home/ubuntu/apache/kafka_2.12-2.5.0/config/zookeeper.properties
-screen -S kafka -m sudo ./kafka-server-start.sh /home/ubuntu/apache/kafka_2.12-2.5.0/config/server.properties
+screen -S zookeeper -dm sudo ./zookeeper-server-start.sh /home/ubuntu/apache/kafka_2.12-2.5.0/config/zookeeper.properties
+screen -S kafka -dm sudo ./kafka-server-start.sh /home/ubuntu/apache/kafka_2.12-2.5.0/config/server.properties
 export SPARK_HOME='/home/ubuntu/apache/spark-2.2.2-bin-hadoop2.7/'
 export PYSPARK_PYTHON=python3
 cd $SPARK_HOME
-screen -S kafka -m sudo bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0 /home/ubuntu/Real-Time-Sentiment-Analysis/tweet_spark_consumer.py
+screen -S kafka -dm sudo bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0 /home/ubuntu/Real-Time-Sentiment-Analysis/tweet_spark_consumer.py
 cd /home/ubuntu/elastic/elasticsearch-6.4.2/
-screen -S elasticsearch -m bin/elasticsearch
+screen -S elasticsearch -dm bin/elasticsearch
 cd /home/ubuntu/elastic/kibana-6.4.2-linux-x86_64/
-screen -S kibana -m bin/kibana
+screen -S kibana -dm bin/kibana
