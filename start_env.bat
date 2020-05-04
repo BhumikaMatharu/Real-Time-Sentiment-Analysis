e88805e713fa2e978d64e03eda23e0675e4ece36
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-05-03 20:57:18 -0700

    Created start.sh

--- start_env.bat
+++ start_env.bat
@@ -8,7 +8,7 @@ START "" python tweet_listener.py
 timeout 15
 START "" C:\Elastic\elasticsearch-6.4.2\bin\elasticsearch.bat
 timeout 15
-CSTART "" %SPARK_HOME%\bin\spark-submit --master local[4] --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0 C:\Users\prath\PycharmProjects\Real-Time-Sentiment-Analysis\tweet_spark_consumer.py
+START "" %SPARK_HOME%\bin\spark-submit --master local[4] --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.0 C:\Users\prath\PycharmProjects\Real-Time-Sentiment-Analysis\tweet_spark_consumer.py
 timeout 10
 START "" C:\Elastic\kibana-6.4.2-windows-x86_64\bin\kibana.bat
 PAUSE
